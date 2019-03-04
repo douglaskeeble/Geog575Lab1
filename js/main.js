5855191ca9bbf4711f5a243783fac2d3efa4e433
Douglas Keeble <douglaskeeble@gmail.com>
2019-03-03 23:26:00 -0700

    V2.0

--- js/main.js
+++ js/main.js
@@ -1,155 +1,235 @@
 	$(document).ready(function() {
 
 		var cities;
+		var coords = [46.8797, -110.3626, 6];
 		var map = L.map('map', {
-			center: [46.8797, -110.3626],
-			zoom: 6,
-			minZoom: 4
+			center: [coords[0], coords[1]],
+			zoom: coords[2],
+			minZoom: 6,
+			zoomControl: false
 		});
 
-		L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png', {
-	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
-	subdomains: 'abcd',
-	maxZoom: 19
+		var basemap = L.tileLayer(
+			'https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png', {
+				attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>, Data sourced from <a href="https://www.census.gov/data/tables/2017/demo/popest/total-cities-and-towns.html">US Census</a>',
+				subdomains: 'abcd',
+				maxZoom: 19
 			}).addTo(map);
 
-      	$.getJSON('data/popData.geojson')
-      		.done(function(data) {
-      			var info = processData(data);
-            createPropSymbols(info.timestamps, data);
-						createSliderUI(info.timestamps);
-      	 	})
-      	.fail(function() { alert('There has been a problem loading the data.')});
+		var esri_WorldImagery = L.tileLayer(
+			'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+				attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
+			});
 
-        	function processData(data) {
-        		var timestamps = [];
-        		var min = Infinity;
-        		var max = -Infinity;
+		$.getJSON('data/popData.geojson')
+			.done(function(data) {
+				var info = processData(data);
+				createPropSymbols(info.timestamps, data);
+				populateBar();
+				createSliderUI(info.timestamps);
+				jqueryInitialize();
+				basemapSwitcher();
+				zoomButtons();
+			})
+			.fail(function() {
+				alert('There has been a problem loading the data.')
+			});
 
-        		for (var feature in data.features) {
+		function processData(data) {
+			var timestamps = [];
+			var min = Infinity;
+			var max = -Infinity;
 
-        			var properties = data.features[feature].properties;
+			for (var feature in data.features) {
+				var properties = data.features[feature].properties;
 
-        			for (var attribute in properties) {
+				for (var attribute in properties) {
 
-        				if ( attribute != 'id' &&
-        				  attribute != 'name' &&
-        				  attribute != 'lat' &&
-        				  attribute != 'lon' ) {
+					if (attribute != 'id' &&
+						attribute != 'name' &&
+						attribute != 'lat' &&
+						attribute != 'lon') {
 
-        					if ( $.inArray(attribute,timestamps) === -1) {
-        						timestamps.push(attribute);
-          				}
+						if ($.inArray(attribute, timestamps) === -1) {
+							timestamps.push(attribute);
+						}
 
-        					if (properties[attribute] < min) {
-        						min = properties[attribute];
-        					}
+						if (properties[attribute] < min) {
+							min = properties[attribute];
+						}
 
-        					if (properties[attribute] > max) {
-        						max = properties[attribute];
-        					}
-        				}
-        			}
-        		}
+						if (properties[attribute] > max) {
+							max = properties[attribute];
+						}
+					}
+				}
+			}
 
-        		return {
-          		timestamps : timestamps,
-        			min : min,
-        			max : max
-        		}
-        	}
+			return {
+				timestamps: timestamps,
+				min: min,
+				max: max
+			}
+		}
 
-          	function createPropSymbols(timestamps, data) {
+		function createPropSymbols(timestamps, data) {
 
-          		cities = L.geoJson(data, {
+			cities = L.geoJson(data, {
 
-          			pointToLayer: function(feature, latlng) {
+				pointToLayer: function(feature, latlng) {
+					return L.circleMarker(latlng, {
+						fillColor: '#B84E14',
+						color: '#341809',
+						weight: 1,
+						fillOpacity: 0.6
+					}).on({
+						mouseover: function(e) {
+							this.openPopup();
+							this.setStyle({
+								color: '#FC600A'
+							});
+						},
+						mouseout: function(e) {
+							this.closePopup();
+							this.setStyle({
+								color: '#341809'
+							});
 
-          			return L.circleMarker(latlng, {
-          				 fillColor: '#B84E14',
-          				 color: '#341809',
-          				 weight: 1,
-          				 fillOpacity: 0.6
-          				}).on({
+						}
+					});
+				}
+			}).addTo(map);
 
-          					mouseover: function(e) {
-          						this.openPopup();
-          						this.setStyle({color: '#FC600A'});
-          					},
-          					mouseout: function(e) {
-          						this.closePopup();
-          						this.setStyle({color: '#341809'});
+			updatePropSymbols(timestamps[0]);
 
-          					}
-          				});
-          			}
-          		}).addTo(map);
+		}
 
-              updatePropSymbols(timestamps[0]);
+		function updatePropSymbols(timestamp) {
 
-          	}
+			cities.eachLayer(function(layer) {
 
-            function updatePropSymbols(timestamp) {
+				var props = layer.feature.properties;
+				var radius = calcPropRadius(props[timestamp]);
+				var percent = props[timestamp] * 100 - 100;
+				var change = 'increased';
+				if (percent < 0) {
+					change = 'decreased';
+				}
+				var popupContent = '<b> Population ' + change + ' by ' + String(percent
+						.toFixed(
+							0)) +
+					'%</b><br>' +
+					'<i>' + props.name +
+					'</i> in </i>' +
+					timestamp + '</i>';
+				layer.setRadius(radius);
+				layer.bindPopup(popupContent, {
+					offset: new L.Point(0, -radius)
+				});
+			});
+		}
 
-              cities.eachLayer(function(layer) {
+		function calcPropRadius(attributeValue) {
+			var scaleFactor = 60;
+			var area = (attributeValue * 100 - 100) * scaleFactor;
+			if (area < 0) {
+				area = 1;
+			}
+			return Math.sqrt(area / Math.PI) * 2;
+		}
 
-			             var props = layer.feature.properties;
-			             var radius = calcPropRadius(props[timestamp]);
-									 var percent = props[timestamp]*100-100;
-			             var popupContent = '<b> Population increased by ' + String(percent.toFixed(0)) +
-				                              '%</b><br>' +
-				                              '<i>' + props.name +
-					                            '</i> in </i>' +
-					                            timestamp + '</i>';
-                  layer.setRadius(radius);
-			            layer.bindPopup(popupContent, { offset: new L.Point(0,-radius) });
-		});
-	 }
-	  function calcPropRadius(attributeValue) {
+		function createSliderUI(timestamps) {
 
-		    var scaleFactor = 60;
-        var area = (attributeValue * 100-100) * scaleFactor;
-        return Math.sqrt(area/Math.PI)*2;
-	}
-
-	function createSliderUI(timestamps) {
-
-		var sliderControl = L.control({ position: 'bottomleft'} );
-
-		sliderControl.onAdd = function(map) {
-
-			var slider = L.DomUtil.create('input', 'range-slider');
+			var slider = L.DomUtil.create('div', 'slider');
 
 			L.DomEvent.addListener(slider, 'mousedown', function(e) {
 				L.DomEvent.stopPropagation(e);
 			});
-
 			$(slider)
-				.attr({'type':'range',
-					'max': timestamps[timestamps.length-1],
-					'min': timestamps[0],
-					'step': 1,
-				  'value': String(timestamps[0])})
-		  		.on('input change', function() {
-		  		updatePropSymbols($(this).val().toString());
-					$('.temporal-legend').text(this.value);
-		  	});
-			return slider;
+				.slider({
+					max: parseInt(timestamps[timestamps.length - 1]),
+					min: parseInt(timestamps[0]),
+					step: 1,
+					value: parseInt(timestamps[0]),
+					slide: function(event, ui) {
+						updatePropSymbols(ui.value.toString());
+						$('.year').html("Year: " + ui.value);
+					}
+
+				});
+			$("#bar").append("<h3 class='year'>Year: 2011</h3>");
+			$('#bar').append(slider);
 		}
 
-		sliderControl.addTo(map);
-		createTemporalLegend(timestamps[0]);
-	}
+		function zoomButtons() {
+			var zoom = L.control({
+				position: 'topright'
+			});
 
-	function createTemporalLegend(startTimestamp) {
-		var temporalLegend = L.control({position: 'bottomleft'});
-
-		temporalLegend.onAdd = function(map) {
-			var output = L.DomUtil.create('output', 'temporal-legend');
-			$(output).text(startTimestamp);
-			return output;
+			zoom.onAdd = function(map) {
+				var buttons = L.DomUtil.create('div', 'zoomButtons');
+				var content = '<ul class="buttons">';
+				content += '<li class="zoomIn"><i class="fas fa-plus"></i></li>';
+				content += '<li class="home"><i class="fas fa-home"></i></li>';
+				content += '<li class="zoomOut"><i class="fas fa-minus"></i></li>';
+				content += '</ul>';
+				$(buttons).append(content);
+				return buttons;
+			}
+			zoom.addTo(map);
+			$(".zoomIn").click(function() {
+				map.zoomIn();
+			});
+			$(".home").click(function() {
+				map.flyTo([coords[0], coords[1]], coords[2]);
+			});
+			$(".zoomOut").click(function() {
+				map.zoomOut();
+			});
 		}
-		temporalLegend.addTo(map);
-	}
+
+		function populateBar() {
+			var contents = '<h3>Cities</h3><ul class="featureList">';
+			cities.eachLayer(function(layer) {
+				contents += '<li id="' + layer.feature.properties.name + '">' + layer.feature
+					.properties.name + '</li>';
+			})
+			contents += '</ul>';
+			$('#bar').append(contents);
+		}
+
+		function jqueryInitialize() {
+			cities.eachLayer(function(layer) {
+				$('#' + layer.feature.properties.name).on({
+					'mouseover': function() {
+						layer.openPopup()
+					},
+					'mouseout': function() {
+						layer.closePopup()
+					}
+				});
+			})
+		}
+
+		function basemapSwitcher() {
+			var baseButton = L.control({
+				position: 'topright'
+			});
+			baseButton.onAdd = function(map) {
+				var output = L.DomUtil.create('div', 'baseButton');
+				$(output).text("Toggle Basemap");
+				return output
+			}
+			baseButton.addTo(map);
+			$('.baseButton').click(function() {
+				if (map.hasLayer(basemap)) {
+					map.removeLayer(basemap);
+					map.addLayer(esri_WorldImagery);
+				} else {
+					map.removeLayer(esri_WorldImagery);
+					map.addLayer(basemap);
+				}
+			})
+		}
 
 	});
